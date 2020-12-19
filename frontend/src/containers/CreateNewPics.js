17ffc7b66762220fedcd1e49e0232629f67c857f
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-12-19 18:44:19 +0600

    Убрал неиспользуемые функции

--- frontend/src/containers/CreateNewPics.js
+++ frontend/src/containers/CreateNewPics.js
@@ -3,7 +3,6 @@ import {Typography, makeStyles, Button} from "@material-ui/core";
 import {useDispatch, useSelector} from "react-redux";
 import FormElement from "../components/UI/FormElement/FormElement";
 import {createNewPic} from "../store/actions/picturesActions";
-import {push} from 'connected-react-router';
 
 const useStyles = makeStyles(() => ({
     form: {
