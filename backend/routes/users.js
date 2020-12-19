e61d63b90e6ed114c84d66b383a9df68c231f500
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-12-19 16:47:32 +0600

    Убрал консоль лог

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -50,7 +50,6 @@ router.delete('/sessions', async (req, res) => {
 });
 
 router.post('/facebookLogin', async (req, res) => {
-    console.log(req.body);
     const inputToken = req.body.accessToken;
     const accessToken = config.facebookAccess + '|' + config.facebookSecret;
     const debugToken = `https://graph.facebook.com/debug_token?input_token=${inputToken}&access_token=${accessToken}`;
