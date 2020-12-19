654a169d57d90a4679f5a757d4bad7969dd9c4c9
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-12-19 15:43:09 +0600

    Добавил страницы в роуты

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,6 +4,8 @@ import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import Layout from "./components/Layout/Layout";
 import Main from "./containers/Main";
+import LoginRegister from "./containers/LoginRegister";
+import CreateNewPics from "./containers/CreateNewPics";
 
 const ProtectedRoute = ({isAllowed, redirectTo, ...props}) => {
     return isAllowed ?
@@ -17,6 +19,27 @@ const Routes = ({user}) => {
             <ToastContainer autoClose={3000} />
             <Switch>
                 <Route path='/' exact component={Main} />
+                <ProtectedRoute
+                    isAllowed={!user}
+                    path='/login'
+                    exact
+                    component={LoginRegister}
+                    redirectTo='/'
+                />
+                <ProtectedRoute
+                    isAllowed={!user}
+                    path='/register'
+                    exact
+                    component={LoginRegister}
+                    redirectTo='/'
+                />
+                <ProtectedRoute
+                    isAllowed={user}
+                    path='/create-new-pic'
+                    exact
+                    component={CreateNewPics}
+                    redirectTo='/'
+                />
                 <Route render={() => <h1 style={{textAlign: 'center'}}>404 Page Not Found</h1>} />
             </Switch>
         </Layout>
